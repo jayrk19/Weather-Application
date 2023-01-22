6f6693da0995a15b5ccc514f2ac120d0fa8724eb
jayrk19 <84495297+jayrk19@users.noreply.github.com>
2023-01-22 23:40:50 +0530

    1 file modified

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,25 +1,20 @@
-
-export default function Output({ lat, setLat, setLong, long }){
-  const fetchingAPI = async () => {
+import React, { useEffect, useState } from 'react';
+export default function Output({ lat, setLat, setLong, long }) {
+  const [data, setData] = useState([]);
+  const fetchingAPI = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=81c2336ccda201c6ed2f51bce57e3bb3`;
-    let d = await fetch(url);
-    let res = await d.json();
-  
-    return res;
-    
-    
+    return fetch(url)
+      .then((response) => response.json())
+      .then((d) => setData(prevData=>[...prevData,d]));
   };
+  useEffect(()=>{
+    fetchingAPI();
+  },[])
+  console.log(data)
 
-  let s= "";
-  const res =  fetchingAPI();
-  res.then(r=>console.log(r))
-  // s = `Weather at ${res.name}, is ${res.sys.country} is ${res.weather[0].description} with temp as ${res.main.temp}`;
-  let name = res.name;
-  console.log(name)
   return (
-  <>
-    <h1>{s}</h1>
-  
-  
-  </>);
+    <>
+      {/* <h1>{s}</h1> */}
+    </>
+  );
 }
